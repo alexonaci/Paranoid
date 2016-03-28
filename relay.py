986573ea583df6368c85fb64a3915012676f25f1
icamaster <mini_cezarus@yahoo.com>
2016-03-28 18:10:39 +0300

    Delete relay.py
    Moved to php (relay.php)

--- relay.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-from time import sleep
-from serial import Serial
-serial_port = '/dev/ttyACM0';
-serial_bauds = 9600;
-s = Serial(serial_port, serial_bauds);
-sleep(2);
-s.write('R');
